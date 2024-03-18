6081df099f6d75482ab5e8756f3c5b1abeb56590
Canlo <canhuynh2k1@gmail.com>
2024-03-18 23:21:21 +0700

    first commit

---
+++ pages/contact_page.php
@@ -0,0 +1,52 @@
+<!--lienhe-->
+<?php include '../templates/header.php' ?>
+<div class="liehHe">
+    <div class="liehHe_left">
+        <h3 class="gioithieu_nhom_11">THÔNG TIN SHOP</h3>
+        <ul class="gioithieu_nhom">
+            <h5>Thành viên Shop :</h5>
+            <li>
+                Huỳnh Văn Can
+            </li>
+            <li>
+                Trần Duy Khải
+            </li>
+            <li>
+                Hồ Quang Đạt
+            </li>
+            <li>
+                Nguyễn Công Huynh
+            </li>
+        </ul>
+        <p class="gioithieu_nhom_p"> <span>Địa chỉ: </span>N3 Điên Biên phủ,Phường 25,Quận Bình Thạnh
+        </p>
+        <ul class="gioithieu_nhom">
+            <h5>Thông tin liên hệ:</h5>
+            <li>
+                <span>Nhóm trưởng: </span> Huỳnh Văn Can
+            </li>
+            <li>
+                <span>Số điện thoại: </span> 0392216028
+            </li>
+            <li>
+                <span>Email: </span> canhuynh2k1@gmail.com
+            </li>
+        </ul>
+        <ul class="gioithieu_nhom">
+            <h5>Thông tin mạng xã hội:</h5>
+            <li>
+                <span>Facebook: </span><a
+                    href="https://www.facebook.com/">https://www.facebook.com/canhuynh123</a>
+            </li>
+            <li>
+                <span>Youtube: </span><a
+                    href="https://www.youtube.com/">https://www.youtube.com/</a>
+            </li>
+
+        </ul>
+    </div>
+    <div class="liehHe_right">
+        <img src="/assets/img/banner/image.png" alt="" style="width: 600px; height: 700px;">
+    </div>
+</div>
+<?php include '../templates/footer.php' ?>
